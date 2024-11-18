fde8f6c524149bf911ae39df8fa50a0dd9c22708
Michal Vizváry <vizvarym1661@gmail.com>
2024-11-18 12:01:11 +0100

    fix: hex color conversion

--- src/utils.cpp
+++ src/utils.cpp
@@ -13,20 +13,36 @@ Color::Color(int red, int green, int blue, int alpha) {
 Color::Color(const std::string& hex) {
     int red, green, blue, alpha = 255;
 
+    // Skontroluj správny formát hex (7 alebo 9 znakov)
     if (hex.size() == 7) {
         std::stringstream ss;
-        ss << std::hex << hex.substr(1);
-        ss >> red >> green >> blue;
+        ss << std::hex << hex.substr(1, 2);  // Čítaj dva znaky pre červenú
+        ss >> red;
+        ss.clear();
+        ss << std::hex << hex.substr(3, 2);  // Čítaj dva znaky pre zelenú
+        ss >> green;
+        ss.clear();
+        ss << std::hex << hex.substr(5, 2);  // Čítaj dva znaky pre modrú
+        ss >> blue;
     }
-    else if (hex.size() == 9) {
+    else if (hex.size() == 9) {  // Ak je formát s alfa kanálom (napr. #RRGGBBAA)
         std::stringstream ss;
-        ss << std::hex << hex.substr(1);
-        ss >> alpha >> red >> green >> blue;
+        ss << std::hex << hex.substr(1, 2);  // Alfa
+        ss >> alpha;
+        ss.clear();
+        ss << std::hex << hex.substr(3, 2);  // Červená
+        ss >> red;
+        ss.clear();
+        ss << std::hex << hex.substr(5, 2);  // Zelená
+        ss >> green;
+        ss.clear();
+        ss << std::hex << hex.substr(7, 2);  // Modrá
+        ss >> blue;
     }
-    else if (hex.size() == 4) {
-        red = std::stoi(hex.substr(1, 1), nullptr, 16) * 17;
-        green = std::stoi(hex.substr(2, 1), nullptr, 16) * 17;
-        blue = std::stoi(hex.substr(3, 1), nullptr, 16) * 17;
+    else if (hex.size() == 4) {  // Skvelé pre skrátený hex formát #RGB
+        red = std::stoi(hex.substr(1, 1), nullptr, 16) * 17;   // Pre #R
+        green = std::stoi(hex.substr(2, 1), nullptr, 16) * 17; // Pre #G
+        blue = std::stoi(hex.substr(3, 1), nullptr, 16) * 17;  // Pre #B
     }
     else {
         throw std::invalid_argument("Invalid hex color format");
