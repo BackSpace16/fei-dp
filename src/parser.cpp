36781a229b5bb240b188ecadbefad4eade28e80c
Michal Vizváry <vizvarym1661@gmail.com>
2024-11-18 12:00:17 +0100

    feat: data parsing xyz file

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,13 +1,40 @@
 #include <main.hpp>
+#include <elements.hpp>
 
-Parser::Parser(const std::string& path) {
-    std::ifstream file(path);
+Atom::Atom(const std::string& element, const std::array<double, 3>& pos) : position(pos), color("#000") {
+    if (elementData.contains(element)) {
+        color = Color{elementData.at(element).color};
+        scale = elementData.at(element).radius;
+    } 
+    else {
+        throw std::runtime_error("Unknown element: " + element);
+    }
+}
+
+
+glm::vec3 Atom::getPositionVec3() const {
+    return glm::vec3(position[0], position[1], position[2]);
+}
+
+glm::vec3 Atom::getColor() const {
+    return color.toVec3();
+}
+
+glm::vec3 Atom::getScale() const {
+    return glm::vec3(scale);
+}
+
+Parser::Parser(const std::string& path) : filePath(path) {}
+
+std::vector<Atom> ParserXYZ::parseFile() {
+    std::vector<Atom> atoms;
+    std::ifstream file(filePath);
     std::string line;
 
     if (file.is_open()) {
         size_t i = 0;
         while (std::getline(file, line)) {
-            std::cout << i << " ";
+            //std::cout << i << " ";
             std::istringstream iss(line);
             std::string word;
             std::vector<std::string> words;
@@ -18,17 +45,26 @@ Parser::Parser(const std::string& path) {
                 std::cout << std::endl;
             }
             else if(i > 1) {
-                for (size_t j = 0; j <= 3; j++) {
+                iss >> word;
+                std::string element = word;
+                std::array<double, 3> position;
+
+                if (word.find_first_of("0123456789") == 0) {
+                    break;
+                }
+
+                for (int j = 0; j < 3; ++j) {
                     iss >> word;
-                    words.push_back(word);
+                    position[j] = std::stod(word);
                 }
 
-                for (const auto& w : words) {
-                    std::cout << w << " ";
+                try {
+                    atoms.emplace_back(element, position);
+                } 
+                catch (const std::runtime_error& e) {
+                    std::cerr << "Chyba pri vytváraní atómu: " << e.what() << std::endl;
                 }
-                std::cout << std::endl;
             }
-
             i++;
         }
         file.close();
@@ -36,11 +72,26 @@ Parser::Parser(const std::string& path) {
     else {
         std::cerr << "Nepodarilo sa otvoriť súbor!" << std::endl;
     }
+    return atoms;
+}
+
+Data::Data(Settings& settings) : settings{settings} {
+    createParser();
+    parse();
+}
+
+void Data::createParser() {
+    parser = std::make_unique<ParserXYZ>("../../input/rutile_Au_Oad_CO.xyz");
+    // F8BT
+    // ethylthiol_on_Au221_pulling
+    // dithioCAB-Au24_hist
+    // rutile_Au_Oad_CO
+    // 0.0-7.0
 }
 
 
-Data::Data(Parser& parser) : parser{parser} {
-
+void Data::parse() {
+    atoms = std::move(parser->parseFile());
 }
 
 /*Data::~Data() {
